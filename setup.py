413e7f83e2e331d880094e2f03d0d0343b0928be
crusopaul <crusopaul@gmail.com>
2026-02-27 18:11:39 -0500

    Update setup.py
    Co-authored-by: Ramona T <ramona.t.me@pm.me>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ EXTRA_DEPENDENCIES = {
     "alchemer": ["surveygizmo"],
     "avro": ["fastavro"],
     "azure": ["azure-storage-blob"],
-    "box": ["boxsdk >= 4"],
+    "box": ["boxsdk[jwt] >= 4"],
     "braintree": ["braintree"],
     "catalist": ["paramiko"],
     "civis": ["civis"],
